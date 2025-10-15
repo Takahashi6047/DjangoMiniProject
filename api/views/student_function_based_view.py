c22ce2aada0a0416665ffccbbe875eac1ed0cfea
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-10-15 17:50:12 +0800

    serializer in api

--- api/views/student_function_based_view.py
+++ api/views/student_function_based_view.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from students.models import student
-
+from ..serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 def studentsView(request):
     students = student.objects.all()
+    serialize = StudentSerializer(students, many=True)
+    return Response(serialize.data, status=status.HTTP_200_OK)
