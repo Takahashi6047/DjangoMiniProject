54f7ef69e96f75db35b3a309d7d9364ca4afe6dd
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-10-15 17:24:32 +0800

    employees migrate and models

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+# @admin.register(Employee)
+# # class EmployeeAdmin(admin.ModelAdmin):
+# #     list_display = ('emp_id', 'emp_name', 'designation')
+# #     search_fields = ('emp_id', 'emp_name', 'designation')
+
+
+admin.site.register(Employee)
