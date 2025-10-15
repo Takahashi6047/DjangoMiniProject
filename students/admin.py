fcea4d6aaf57d380a392b60b9442f72d540481d8
John Robert G. Adlawan <johnrobert.adlawan@msunaawan.edu.ph>
2025-10-15 16:53:58 +0800

    student migrations

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import student
 
 # Register your models here.
+@admin.register(student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'name', 'branch')
+    search_fields = ('student_id', 'name', 'branch')
